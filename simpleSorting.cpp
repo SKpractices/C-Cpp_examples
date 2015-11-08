b39ccdd35cfce7a56b790a510d00bb39cece0fcd
Sourabh Kulhare <skrealworld@users.noreply.github.com>
2015-11-08 03:44:35 -0500

    Small code.

--- simpleSorting.cpp
+++ simpleSorting.cpp
@@ -3,31 +3,41 @@ Simple code to sort array of numbers. This will contain all major sorting algori
 Methode 1 - Simple get the max and store it. 
 */
 
-
 #include <iostream>
 
 using namespace std;
 
     // declare the functions
     //int getArray(int num_ele);
-    int max(int a[]);
-    int *stripArray(int a[]);
+    //int max(int a[]);
+
     int main()
     {
     int a[7] = {10,2,10,30,50,12,14};
-    cout<<sizeof(a)/sizeof(*a)<<"\n";
-    int *b;
-    b=  stripArray(a);
-    cout<<b[3];
+    cout<<"Size of given array is :- ";
+    int len = sizeof(a)/sizeof(*a);
+    cout<<len<<"\n";
+    
+    for(int i=len ; i>=1; i--)
+    {
+        for(int j = 0; j<i ; j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                int temp;
+                temp = a[j+1];
+                a[j+1] = a[j];
+                a[j] = temp;
+            }
+        }
+        cout<<a[i-1];
+        cout<<"\n";
+    }
 
-
-
-        //a=getArray(4);
-    //cout<<a;
     return 0;
     }
 
-    // function to get the maximum of an array.
+/*
     int max(int a[])
     {
     for(int i=0; i<7 ; i++)
@@ -41,17 +51,7 @@ using namespace std;
     return a[6];
     }
 
-    int *stripArray(int a[])
-    {
-        int len = (sizeof(a)/sizeof(*a))-1;
-        int b[len-1];
-        for(int i=0;i<len-1;i++)
-        {
-            b[i] = a[i];
-        }
-        return b;
-    }
-/*
+
 int getArray(int num_ele)
 {
     int a[num_ele];
@@ -65,3 +65,13 @@ int getArray(int num_ele)
 }
 
 */
+
+
+
+
+
+
+
+
+
+
