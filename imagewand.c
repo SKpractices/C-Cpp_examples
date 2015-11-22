44fb11c875bad2494b1b5f03976818ae7aed36fa
Sourabh Kulhare <skrealworld@users.noreply.github.com>
2015-11-22 14:07:28 -0500

    Cleaned lit.

--- imagewand.c
+++ imagewand.c
@@ -38,7 +38,7 @@ int main(int argc, const char * argv[])
     pwand_ip = NewPixelWand();
     
     // Read image and check its existance.
-    img_Check =  MagickReadImage(wand_ip, "/Users/SK_Mac/Downloads/image3.jpg");
+    img_Check =  MagickReadImage(wand_ip, "/Users/SK_Mac/Downloads/image2.jpg");
     
     if(img_Check==MagickFalse)
     {
@@ -60,9 +60,10 @@ int main(int argc, const char * argv[])
     height1 = MagickGetImageHeight(wand_ip);
     //printf("Height is %lu",height1);
 
+    height2 =  2000;
+    width2 =   1750;
     
-    double img_IP[height1][width1];
-
+    unsigned char img_IP[height1][width1];
     for (int i=0 ; i<height1;i++)
     {
         //Get the (i+1) th row of image
@@ -72,23 +73,18 @@ int main(int argc, const char * argv[])
         {
             //Get the each pixel values.
             img_IP[i][j]=255*PixelGetRed(pixels_ip[j]);
-            //printf("%f\n",img_IP[i][j]);
-
         }
     
     }
     
-    height2 =  2500;
-    width2 =   2500;
+
     
     // Bilineat linterpolation. 
     
     unsigned char img_op[height2][width2];
     
-    float scale_h = (float) height1/(float)height2;
-    //printf("Scale with Height is %f\n",scale_h);
-    float scale_w = (float) width1/(float)width2;
-    //printf("Scale with Width is %f\n",scale_w);
+    float scale_h = height1/(float)height2;
+    float scale_w = width1/(float)width2;
 
     
     
@@ -103,23 +99,13 @@ int main(int argc, const char * argv[])
             width_idx =  (int) width_Frac_Idx;
             float del_h = height_Frac_Idx - height_idx;
             float del_w = width_Frac_Idx - width_idx;
-            //printf("%f\n",del_w);
-            
 
             img_op[i][j] = (img_IP[height_idx][width_idx])*(1 - del_h)*(1-del_w) + (img_IP[height_idx+1][width_idx])*(del_h)*(1-del_w) + (img_IP[height_idx][width_idx+1])*(1 - del_h)*(del_w) + (img_IP[height_idx+1][width_idx+1])*(del_h)*(del_w);
-            
-            
-            
-            //printf("%d\n",img_op[height_idx][width_idx]);
-            
-            
             
         }
         
         
     }
-    
-    
     
     
     // Initialize the wand for final image based one the given height and width.
@@ -137,36 +123,30 @@ int main(int argc, const char * argv[])
     MagickNewImage(wand_op,width2,height2,p_wand);
     iterator_op=NewPixelIterator(wand_op);
     PixelWand **pixels_op;
-    int i1,j1;
-    for(i1=0;i1<height2;i1++)
+    for(int i=0;i<height2;i++)
     {
         // Get the next row of the image as an array of PixelWands
         pixels_op= PixelGetNextIteratorRow(iterator_op,&width2);
         // Set the row of wands to a simple gray scale gradient
-        for(j1=0;j1<width2;j1++)
+        for(int j=0;j<width2;j++)
         {
             //PixelSetColorFromWand(wand_op, pixel_op[j1]);
             //unsigned char temp = img_op[i1][j1];
-            sprintf(hex,"#%02x%02x%02x",img_op[i1][j1],img_op[i1][j1],img_op[i1][j1]);
-            PixelSetColor(pixels_op[j1],hex);
+            sprintf(hex,"#%02x%02x%02x",img_op[i][j],img_op[i][j],img_op[i][j]);
+            PixelSetColor(pixels_op[j],hex);
         }
         // Sync writes the pixels back to the m_wand
         PixelSyncIterator(iterator_op);
     }
+    
+    
+    
     MagickWriteImage(wand_op,"bits_demo.gif");
-    
-    
-    
-    
-
     //Display the input image.
     //MagickDisplayImage(wand_ip, ":0");
     
     //Display the output image.
     MagickDisplayImage(wand_op, ":0");
-
-    
-    
     wand_ip = DestroyMagickWand(wand_ip);
     wand_op = DestroyMagickWand(wand_op);
     MagickWandTerminus();
