193fef02751977290a2b3eb2205e87e47acd4efb
Sourabh Kulhare <skrealworld@users.noreply.github.com>
2015-11-21 21:15:55 -0500

    70% working.

--- imagewand.c
+++ imagewand.c
@@ -12,14 +12,13 @@
 #include <math.h>
 
 
-
 int main(int argc, const char * argv[])
 {
     MagickWandGenesis();
     
     
     // Declearation of variables
-        // Wand variables
+    // Wand variables
     PixelWand *pwand_ip;
     PixelIterator *iterator_ip,*iterator_op;
     PixelWand **pixels_ip;
@@ -39,7 +38,7 @@ int main(int argc, const char * argv[])
     pwand_ip = NewPixelWand();
     
     // Read image and check its existance.
-    img_Check =  MagickReadImage(wand_ip, "/Users/SK_Mac/Downloads/image1.jpg");
+    img_Check =  MagickReadImage(wand_ip, "/Users/SK_Mac/Downloads/image3.jpg");
     
     if(img_Check==MagickFalse)
     {
@@ -56,7 +55,11 @@ int main(int argc, const char * argv[])
     // get the height and width
     
     width1 =  MagickGetImageWidth(wand_ip);
+    //printf("Width is %lu\n",width1);
+    
     height1 = MagickGetImageHeight(wand_ip);
+    //printf("Height is %lu",height1);
+
     
     double img_IP[height1][width1];
 
@@ -70,24 +73,24 @@ int main(int argc, const char * argv[])
             //Get the each pixel values.
             img_IP[i][j]=255*PixelGetRed(pixels_ip[j]);
             //printf("%f\n",img_IP[i][j]);
-            //gray_val[i][j] = PixelGetAlpha(pixels_ip[j]);
-            //gray_val[i][j] = PixelGetOpacity(pixels_ip[j]);
-            //wand_ip(GetPixelY(pixels_ip[j]);
-            //printf("%d\n",img_IP[i][j]);
 
         }
     
     }
     
-    height2 =  500;
-    width2 =   500;
+    
+    height2 =  800;
+    width2 =   800;
     
     // Bilineat linterpolation. 
     
-    unsigned char img_op[height2][width2];
+    double img_op[height2][width2];
     
     float scale_h = (float) height1/(float)height2;
+    //printf("Scale with Height is %f\n",scale_h);
     float scale_w = (float) width1/(float)width2;
+    //printf("Scale with Width is %f\n",scale_w);
+
  
     
     for (int i = 0 ; i <height2 ; i++)
@@ -95,14 +98,18 @@ int main(int argc, const char * argv[])
         for (int j = 0 ; j< width2 ; j++)
         {
             float height_Frac_Idx = i*scale_h;
-            float width_Frac_Idx = i*scale_w;
+            float width_Frac_Idx = j*scale_w;
             int height_idx, width_idx;
             height_idx = (int) height_Frac_Idx;
             width_idx =  (int) width_Frac_Idx;
             float del_h = height_Frac_Idx - height_idx;
             float del_w = width_Frac_Idx - width_idx;
+            //printf("%f\n",del_w);
             
-            img_op[i][j] = (unsigned char)(img_IP[height_idx][width_idx]*(1 - del_h)*(1-del_w) + img_IP[height_idx+1][width_idx]*(del_h)*(1-del_w) + img_IP[height_idx][width_idx+1]*(1 - del_h)*(del_w) +img_IP[height_idx+1][width_idx+1]*(del_h)*(del_w));
+
+            img_op[i][j] = (img_IP[height_idx][width_idx])*(1.00 - del_h)*(1.00-del_w) + (img_IP[height_idx+1][width_idx])*(del_h)*(1.00-del_w) + (img_IP[height_idx][width_idx+1])*(1.00 - del_h)*(del_w) + (img_IP[height_idx+1][width_idx+1])*(del_h)*(del_w);
+            
+            
             
             //printf("%d\n",img_op[height_idx][width_idx]);
             
@@ -128,7 +135,7 @@ int main(int argc, const char * argv[])
     PixelSetColor(p_wand,"white");
     
     //Configure the output image wand.
-    MagickNewImage(wand_op,height2,width2,p_wand);
+    MagickNewImage(wand_op,width2,height2,p_wand);
     iterator_op=NewPixelIterator(wand_op);
     PixelWand **pixels_op;
     int i1,j1;
@@ -137,9 +144,10 @@ int main(int argc, const char * argv[])
         // Get the next row of the image as an array of PixelWands
         pixels_op= PixelGetNextIteratorRow(iterator_op,&width2);
         // Set the row of wands to a simple gray scale gradient
-        for(j1=0;j1<width2;j1++) {
+        for(j1=0;j1<width2;j1++)
+        {
             //PixelSetColorFromWand(wand_op, pixel_op[j1]);
-            int temp = img_IP[i1][j1];
+            unsigned char temp = img_op[i1][j1];
             sprintf(hex,"#%02x%02x%02x",temp,temp,temp);
             PixelSetColor(pixels_op[j1],hex);
         }
@@ -168,7 +176,7 @@ int main(int argc, const char * argv[])
     {
         for(int j = 1; j<10; j++ )
         {
-            printf("%d \t", img_op[i][j]);
+            //printf("%d \t", img_op[i][j]);
             
         }
     }
