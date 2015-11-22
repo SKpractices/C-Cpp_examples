d44c39410a53281fdd728ab30325c2888d6a823e
Sourabh Kulhare <skrealworld@users.noreply.github.com>
2015-11-22 16:18:40 -0500

    return 2D array from function.

--- 2d_array.c
+++ 2d_array.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 
-int main () 
-{
 
-   /* a pointer to an int */
-   
-   int arr[3][4];
-   for (int i = 0; i < 3; i++ ) 
-   {
-      for (int j = 0 ; j< 4; j++)
-      {
-        arr[i][j] = i*j;
-      //printf( "r[%d][%d]: %d\n", i,j,*((*(p +i) + j));
-      }
-   }
+int (*(Matrix_sum)(int matrix1[][3], int matrix2[][3]))[3]{
+    int i, j;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            matrix1[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+    return matrix1;
+}
 
-for (int i = 0; i < 3; i++ ) 
-   {
-      for (int j = 0 ; j< 4; j++)
-      {
-      printf("%d",*((*(arr +i) + j)));
-      }
-   }
-   return 0;
+int main(){
+    int x[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int y[3][3] = {{7,8,9},{4,5,6},{1,2,3}};
+    
+    int (*a)[3]; //pointer to an array
+    int i,j;
+  
+    a = Matrix_sum(x,y); //asigning
+    printf("The sum of the matrix is: \n");
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            printf("%d",a[i][j]);
+            printf("\t");
+        }
+        printf("\n");
+    }
+    return 0;
 }
