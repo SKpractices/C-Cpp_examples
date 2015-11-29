e41411d09bdc97c415afebd6613b3cee611af5c2
Sourabh Kulhare <skrealworld@users.noreply.github.com>
2015-11-29 02:57:01 -0500

    added fixed point operations.

--- imagewand.c
+++ imagewand.c
@@ -9,6 +9,15 @@
 #include <stdio.h>
 #include <wand/MagickWand.h>// ImageMagick's C framework for image processing.
 #include <time.h>
+#include <stdint.h>
+
+
+// Deifne macros for fixed point conversion
+
+#define SCALE 16
+#define Float2Fixed(x) (x (float)(1<<SCALE)) 
+#define Fixed2Float(x) (x(float)/(float)(1<<SCALE))
+
 
 /*image structure.
     gray_Data = Pointer to store buffer of grayscale values.
@@ -50,7 +59,7 @@ struct image get_Image(char name[])
     
     //Allocate space for gray scale values.
     size_t total_gray_pixels =  height1*width1;
-    unsigned char * image_Buff = malloc(total_gray_pixels);
+    unsigned char * image_Buff = (unsigned char *)malloc(total_gray_pixels*sizeof(unsigned char));
     
     //Read grayscale values ans store them into memory buffer.
     MagickExportImagePixels(wand_ip,   // Image instance
@@ -76,7 +85,7 @@ struct image get_Image(char name[])
 struct image biLinearInterPolate(struct image img1, int h2, int w2)
 {
     struct image img2;
-    img2.gray_Data = malloc(h2*w2*sizeof(unsigned char));
+    img2.gray_Data = (unsigned char *)malloc(h2*w2*sizeof(unsigned char));
     img2.height = h2;
     img2.width = w2;
     
@@ -84,33 +93,33 @@ struct image biLinearInterPolate(struct image img1, int h2, int w2)
     {
         for (int j=0;j<w2;j++)
         {
-            float height_Frac_Idx = i*(img1.height/(float)h2);
-            float width_Frac_Idx =  j*(img1.width/(float)w2);
-            int height_idx, width_idx;
-            height_idx = (int) height_Frac_Idx;
-            width_idx =  (int) width_Frac_Idx;
-            unsigned char p1,p2,p3,p4;
-            int f1,f2,f3,f4;
             
-            p1 = img1.gray_Data[(height_idx*img1.width)+width_idx];
-            p2 = img1.gray_Data[((height_idx+1)*img1.width)+width_idx];
-            p3 = img1.gray_Data[(height_idx*img1.width)+(width_idx+1)];
-            p4 = img1.gray_Data[((height_idx+1)*img1.width)+(width_idx+1)];
-            
-            
-            float del_x,del_y,del_x1,del_y1;
-            
-            del_x = height_Frac_Idx - height_idx;
-            del_y = width_Frac_Idx - width_idx;
-            del_x1 = 1.0f - del_x;
-            del_y1 = 1.0f - del_y1;
+            float height_Frac_Idx = ((i*img1.height)/(float)h2);
+            float width_Frac_Idx =  ((j*img1.width)/(float)w2);
+
+            int height_idx = (int) height_Frac_Idx;
+            int width_idx =  (int) width_Frac_Idx;
         
-            f1 = del_x1*del_y1*256.0f;
-            f2 = del_x*del_y1*256.0f;
-            f3 = del_x1*del_y*256.0f;
-            f4 = del_y*del_x*256.0f;
+            
+            unsigned char p1 = img1.gray_Data[((height_idx*img1.width)+width_idx)];
+            unsigned char p2 = img1.gray_Data[((height_idx+1)*img1.width)+width_idx];
+            unsigned char p3 = img1.gray_Data[(height_idx*img1.width)+(width_idx+1)];
+            unsigned char p4 = img1.gray_Data[((height_idx+1)*img1.width)+(width_idx+1)];
+    
+            
+            float del_h = height_Frac_Idx - height_idx;
+            float del_w = width_Frac_Idx - width_idx;
+            float del_h1 = 1.0f - del_h;
+            float del_w1 = 1.0f - del_w;
         
-            img2.gray_Data[i*w2+j] = (p1*f1 + p2+f2 + p3*f3 + p4*f4)>>8;
+   
+            int f1 = del_h1*del_w1*256.0f;
+            int f2 = del_w*del_h1*256.0f;
+            int f3 = del_h*del_w1*256.0f;
+            int f4 = del_h*del_w*256.0f;
+            
+            
+            img2.gray_Data[i*w2+j] = (p1*f1 + p2*f2 + p3*f3 + p4*f4)>>8;
         }
     }
 
@@ -147,16 +156,7 @@ int main(int argc,  char * argv[])
     
     
     struct image img_OP = biLinearInterPolate(image_IP, height2, width2);
-    
-    
-    
-    
-    //unsigned long height2 = 2500;
-    //unsigned long width2 = 2000;
-
-    
-    
-    
+  
     //Initialize the wand for output window.
     
     MagickWandGenesis();
@@ -171,7 +171,7 @@ int main(int argc,  char * argv[])
     //Set the initial color, it is necessary in MagicWand to have it.
     PixelSetColor(p_wand,"white");
     //Configure the output image wand.
-    MagickNewImage(wand_op,width2,height2,p_wand);
+    MagickNewImage(wand_op,(unsigned long)width2,(unsigned long)height2,p_wand);
     iterator_op=NewPixelIterator(wand_op);
     PixelWand **pixels_op;
     for(int i=0;i<height2;i++)
@@ -179,12 +179,12 @@ int main(int argc,  char * argv[])
         // Get the next row of the image as an array of PixelWands
         pixels_op= PixelGetNextIteratorRow(iterator_op,&w2_temp);
         // Set the row of wands to a simple gray scale gradient
-        for(int j=0;j<width2;j++)
+        for(int j=0;j<w2_temp;j++)
         {
             /*formating RGB values to hex, as these are gray scale values so assgning R,G,B same values.
              It is the way MagicWand render an image.
              */
-            unsigned char temp_char = img_OP.gray_Data[(i*img_OP.width)+j];
+            unsigned char temp_char = img_OP.gray_Data[(i*w2_temp)+j];
             sprintf(hex,"#%02x%02x%02x",temp_char,temp_char,temp_char);
             PixelSetColor(pixels_op[j],hex);
         }
